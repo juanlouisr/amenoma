63e6e49e0f2c70b39371835d965e1f0581923197
mizuday <13519075@std.stei.itb.ac.id>
2022-06-16 15:03:27 +0700

    feat: main model

---
+++ src/models/main.model.ts
@@ -0,0 +1,28 @@
+export class LoadResponse {
+  constructor(
+    public url: string,
+    public name: string,
+    public data: ChapterData[],
+    public author?: string,
+    public posterUrl?: string,
+    public synopsis?: string,
+    public tags?: string[],
+    public status: Status = Status.NULL
+  ) {}
+}
+
+export enum Status {
+  NULL = 0,
+  ONGOING = 1,
+  COMPLETE = 2,
+  PAUSE = 3,
+  DROPPED = 4,
+}
+
+export class ChapterData {
+  constructor(
+    public name: string,
+    public url: string,
+    public dateOfRelease?: string
+  ) {}
+}
