ed7104bcbb3ff1eea745a43b84a3e1aade3c315a
ariaberlian <aria.berlian11@gmail.com>
2022-06-16 18:47:52 +0700

    feat: toggle burger menu

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,61 @@
 <script setup lang="ts">
 import SideBar from "@/components/SideBar.vue";
 import ControlBar from "@/components/ControlBar.vue";
+import NavBar from "@/components/NavBar.vue";
+import { ref } from "@vue/reactivity";
+
+var showSideControlBar = ref<boolean>();
 </script>
 
 <template>
   <div class="flex m-0 p-0 ab">
-    <SideBar />
-    <ControlBar />
-    <RouterView />
+    <Transition name="sidebar">
+      <SideBar v-show="showSideControlBar" class="z-10" />
+    </Transition>
+    <Transition name="controlbar">
+      <ControlBar v-show="showSideControlBar" class="z-0" />
+    </Transition>
+    <div class="flex-1 h-screen contain">
+      <NavBar
+        @toggle-side-control-bar="(show) => (showSideControlBar = show)"
+      ></NavBar>
+      <RouterView />
+    </div>
   </div>
 </template>
+
+<style>
+.sidebar-enter-active {
+  transition: all 0.1s ease-in;
+}
+
+.sidebar-leave-active {
+  transition: all 0.1s ease-in;
+  transition-delay: 0.1s;
+}
+
+.sidebar-enter-from {
+  transform: translateX(-100%);
+}
+
+.sidebar-leave-to {
+  transform: translateX(-100%);
+}
+
+.controlbar-enter-active {
+  transition: all 0.1s ease-in;
+  transition-delay: 0.1s;
+}
+
+.controlbar-leave-active {
+  transition: all 0.1s ease-in;
+}
+
+.controlbar-enter-from {
+  transform: translateY(-100%);
+}
+
+.controlbar-leave-to {
+  transform: translateY(-100%);
+}
+</style>
--- src/components/SideBarItem.vue
+++ src/components/SideBarItem.vue
@@ -3,27 +3,29 @@ import { computed, defineComponent } from "@vue/runtime-core";
 import { useRoute } from "vue-router";
 
 export default defineComponent({
-  props:{
-    src : {type: String},
-    title :{type: String},
-    type :{type: String},
+  props: {
+    src: { type: String },
+    title: { type: String },
+    type: { type: String },
   },
-  setup(props){
-    const route = useRoute()
-    const isActive = computed(() => route.name == props.type)
-    console.log(isActive)
-    return {isActive}
-  }
-})
-
+  setup(props) {
+    const route = useRoute();
+    const isActive = computed(() => route.name == props.type);
+    console.log(isActive);
+    return { isActive };
+  },
+});
 </script>
 
 <template>
-
   <router-link :to="{ name: type }">
-    <div class="relative flex items-center justify-center h-12 w-12 my-2 mr-2 group">
-      <div class="active-link" :class="{ active : isActive, inactive : !isActive}">
-      </div>
+    <div
+      class="relative flex items-center justify-center h-12 w-12 my-2 mr-2 group"
+    >
+      <div
+        class="active-link"
+        :class="{ active: isActive, inactive: !isActive }"
+      ></div>
       <img v-bind:src="src" class="sidebar-icon" v-bind:title="title" />
       <!-- <div class="sidebar-tooltip group-hover:scale-100">{{ title }}</div> -->
     </div>
@@ -31,17 +33,17 @@ export default defineComponent({
 </template>
 
 <style scoped>
-.active-link{
+.active-link {
   float: left;
   padding-right: 0.4rem;
   margin-right: 0.1rem;
   background-color: white;
   border-radius: 0 25px 25px 0;
 }
-.active{
+.active {
   height: 90%;
 }
-.inactive{
+.inactive {
   height: 10%;
 }
 </style>
