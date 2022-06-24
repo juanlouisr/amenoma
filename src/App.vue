36085dfd5558c98ccb79cd73bd11faff61bc79ef
ariaberlian <aria.berlian11@gmail.com>
2022-06-24 16:48:46 +0700

    ngubah showsidebar jadi state mene

--- src/App.vue
+++ src/App.vue
@@ -3,23 +3,23 @@ import SideBar from "@/components/SideBar.vue";
 import ControlBar from "@/components/ControlBar.vue";
 import NavBar from "@/components/NavBar.vue";
 import { ref } from "@vue/reactivity";
+import { usePreferencesStore } from "./stores/preferences";
 
-var showSideControlBar = ref<boolean>();
+const preferences = usePreferencesStore();
+
 </script>
 
 <template>
   <div class="flex m-0 p-0">
     <Transition name="sidebar">
-      <SideBar v-show="showSideControlBar" class="z-10" />
+      <SideBar v-show="preferences.isShowControlSideBar" class="z-10" />
     </Transition>
     <Transition name="controlbar">
-      <ControlBar v-show="showSideControlBar" class="z-0" />
+      <ControlBar v-show="preferences.isShowControlSideBar" class="z-0" />
     </Transition>
     <div class="flex-grow h-screen flex flex-col overflow-x-hidden min-w-0">
-      <NavBar
-        @toggle-side-control-bar="(show) => (showSideControlBar = show)"
-      ></NavBar>
-      <RouterView kirim showSidecontrolBar/>
+      <NavBar @toggle-side-control-bar="() => (preferences.toggleControlSideBar())"></NavBar>
+      <RouterView kirim showSidecontrolBar />
     </div>
   </div>
 </template>
