31afebdff6663b70a3475768bc656266e90335fe
Juan Louis <13519075@std.stei.itb.ac.id>
2022-07-01 03:09:36 +0700

    feat: add icon pwa

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,38 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), VitePWA({ registerType: "autoUpdate" })],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        theme_color: "#202225",
+        name: "Anime Novel Manga",
+        short_name: "AmeNoMa",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#202225",
+        icons: [
+          {
+            src: "icon-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
