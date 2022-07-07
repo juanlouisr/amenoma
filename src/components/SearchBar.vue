f6f04adaa734c380cdb801a2567e812da1b3300d
ariaberlian <aria.berlian11@gmail.com>
2022-07-07 23:33:26 +0700

    chore: lint novelcard searchbar component

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,23 +1,35 @@
 <script setup lang="ts">
-const prop = defineProps<{
-    submit: any,
-    query: string,
-    searching: boolean,
-    input: any
+defineProps<{
+  submit: () => void;
+  query: string;
+  searching: boolean;
+  input: (event: Event) => void;
 }>();
 </script>
 
 <template>
-    <form @submit.prevent="submit()" class="searchbar w-full md:w-2/3 mx-auto flex relative mb-1">
-        <div v-if="searching" class="loading absolute left-3 top-1.5 animate-spin h-7 w-7">
-            <img src="../assets/loading.png" alt="loading" class="object-fit" />
-        </div>
-        <img v-else src="../assets/search.png" alt="search" class="absolute left-2 top-1.5 h-4/6" loading="lazy" />
-        <input class="rounded-full w-full h-11 text-xl shadow-lg overflow-x-hidden outline-none bg-gray-400 text-gray-700
-            px-12
-            " :value="query" @input="input($event)" autofocus />
-    </form>
+  <form
+    @submit.prevent="submit()"
+    class="searchbar w-full md:w-2/3 mx-auto flex relative mb-1"
+  >
+    <div
+      v-if="searching"
+      class="loading absolute left-3 top-1.5 animate-spin h-7 w-7"
+    >
+      <img src="../assets/loading.png" alt="loading" class="object-fit" />
+    </div>
+    <img
+      v-else
+      src="../assets/search.png"
+      alt="search"
+      class="absolute left-2 top-1.5 h-4/6"
+      loading="lazy"
+    />
+    <input
+      class="rounded-full w-full h-11 text-xl shadow-lg overflow-x-hidden outline-none bg-gray-400 text-gray-700 px-12"
+      :value="query"
+      @input="input($event)"
+      autofocus
+    />
+  </form>
 </template>
-
-
- 
--- src/views/novel/HomeView.vue
+++ src/views/novel/HomeView.vue
@@ -5,6 +5,7 @@ import { useNovelStore } from "@/stores/novel";
 import { useDataStore } from "@/stores/data";
 import type { SearchResponse } from "@/models/main.model";
 import SearchBar from "../../components/SearchBar.vue";
+import NovelCard from "../../components/NovelCard.vue";
 
 const query = ref("");
 const searching = ref(false);
@@ -41,56 +42,34 @@ const searchNovel = () => {
 const inputEvent = (event: Event) => {
   query.value = (event.target as HTMLInputElement).value;
 };
-
-const truncate = (text: string, stop: number) => {
-  return text.slice(0, stop) + (stop < text.length ? "..." : "");
-};
 </script>
 
 <template>
   <div class="home flex flex-col">
     <h1 class="title my-3">{{ data.provider }}</h1>
 
-    <SearchBar :submit=searchNovel :searching=searching :query=query :input=inputEvent />
+    <SearchBar
+      :submit="searchNovel"
+      :searching="searching"
+      :query="query"
+      :input="inputEvent"
+    />
 
-    <div v-if="searchResult.length"
-      class="w-[97%] p-2 bg-gray-800 flex flex-wrap my-4 mx-auto py-4 gap-y-5 gap-x-5 justify-center rounded-md justify-items-center">
+    <div
+      v-if="searchResult.length"
+      class="w-[97%] p-2 bg-gray-800 flex flex-wrap my-4 mx-auto py-4 gap-y-5 gap-x-5 justify-center rounded-md justify-items-center"
+    >
       <div v-for="(res, idx) in searchResult" :key="idx">
-        <router-link :to="`/novel/${provider}/${res.nameRoute}`">
-          <div class="res-card h-64 w-40 flex flex-col relative rounded-md">
-            <div class="rating flex flex-row w-4/12 absolute" v-if="res.rating ?? false">
-              <h1 class="rating-score ml-3 mr-0.5">
-                {{ res.rating?.toString }}
-              </h1>
-              <img src="../../assets/star.png" alt="star" class="h-3/5 my-auto" />
-            </div>
-            <img :src="res.posterUrl ?? '#'" alt="poster" class="h-4/6 w-full object-fill rounded-top-md" />
-            <h1 class="text-sm p-1">{{ truncate(res.name, 32) }}</h1>
-            <h3 class="text-xs px-1 absolute bottom-3" v-if="res.latestChapter ?? false">
-              Chapter {{ res.latestChapter }}
-            </h3>
-          </div>
-        </router-link>
+        <NovelCard
+          :link="`/novel/${provider}/${res.nameRoute}`"
+          :rating="res.rating"
+          :name="res.name"
+          :posterurl="res.posterUrl"
+          :latest-chapter="res.latestChapter"
+        />
       </div>
     </div>
+
     <div class="homepage"></div>
   </div>
 </template>
-
-<style scoped>
-input {
-  padding: 0.25rem 2rem 0.25rem 3.5rem;
-}
-
-.rounded-top-md {
-  border-radius: 0.375rem 0.375rem 0 0;
-}
-
-.res-card {
-  background-color: #202225;
-}
-
-.rating {
-  background-color: rgba(32, 34, 37, 0.88);
-}
-</style>
